824b80676f081c0433d03d53afa962ff07478420
sakshi jagtap <144527850+sakshijagtap1312@users.noreply.github.com>
2024-01-02 22:19:57 +0530

    Add files via upload

---
+++ snake water gun.py
@@ -0,0 +1,48 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      jagta
+#
+# Created:     02-01-2024
+# Copyright:   (c) jagta 2024
+# Licence:     <your licence>
+#----------------------------------------------------------------------------
+
+import random
+
+def check(comp, user):
+  if comp ==user:
+    return 0
+
+  if(comp == 0 and user ==1):
+    return -1
+
+  if(comp == 1 and user ==2):
+    return -1
+
+  if(comp == 2 and user == 0):
+    return -1
+
+  return 1
+
+
+comp = random.randint(0, 2)
+user = int(input("0 for Snake, 1 for water and 2 for Gun:\n"))
+
+score = check(comp, user)
+
+print("You: ", user)
+print("Computer: ", comp)
+
+if(score == 0):
+  print("Its a draw")
+elif (score == -1):
+  print("You Lose")
+else:
+  print("You Won")
+
+
+
+
+
